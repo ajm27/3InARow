78e6084ca3c6800ac3df59659450bce8d37cc519
ajm27 <ajm27@njit.edu>
2017-08-12 00:01:23 -0400

    Master Commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	const int BOARD_SIZE_X = 5;
+	const int BOARD_SIZE_Y = 6;
+	const int BOARD_WRAP = 6;
+	const float OFFSET 	   = 0.51f;
+
+	public List<GameObject> orbs;
+	GameObject orb_red, orb_blue, orb_green, orb_yellow;
+
+	public Vector2 mousePosition;
+	public GameObject selectedOrb;
+	public Orb selectedOrb_S;
+	public GameObject switchOrb;
+	public Orb switchOrb_S;
+
+	// Use this for initialization
+	void Start ()
+    {
+		orbs = new List<GameObject> ();
+		orb_red = (GameObject) Resources.Load ("Prefabs/Orb_Red");
+		orb_blue = (GameObject) Resources.Load ("Prefabs/Orb_Blue");
+		orb_green = (GameObject) Resources.Load ("Prefabs/Orb_Green");
+		orb_yellow = (GameObject) Resources.Load ("Prefabs/Orb_Yellow");
+
+		generateBoard ();
+		displayBoard ();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+		if(selectedOrb != null)
+		{
+			selectedOrb_S.setPosition (mousePosition);
+		}
+
+        if(switchOrb != null)
+        {
+            switchOrbs(selectedOrb_S, switchOrb_S);
+        }
+	}
+
+	GameObject getRandomOrb(int index)
+	{
+		int rand = Random.Range (1, 5);
+		GameObject tmp;
+
+		switch (rand) 
+		{
+		case 1:
+				tmp = Instantiate (orb_red);
+				tmp.GetComponent<Orb> ().setOrb(index, "red");
+				return tmp;
+			case 2: 
+				tmp = Instantiate (orb_blue);
+				tmp.GetComponent<Orb> ().setOrb(index, "blue");
+				return tmp;
+			case 3: 
+				tmp = Instantiate (orb_green);
+				tmp.GetComponent<Orb> ().setOrb(index, "green");
+				return tmp;
+			case 4: 
+				tmp = Instantiate (orb_yellow);
+				tmp.GetComponent<Orb> ().setOrb(index, "yellow");
+				return tmp;
+		}
+
+		Debug.Log ("There was an issue returning an orb.");
+		return null;
+	}
+
+	void generateBoard ()
+	{
+		for (int i = 0; i < (BOARD_SIZE_X * BOARD_SIZE_Y); i++) 
+		{
+			orbs.Add (getRandomOrb(i));
+		}
+	}
+
+	int getY (int index)
+	{
+		int tmp = 0;
+
+		while (index > BOARD_WRAP) 
+		{
+			index = index - BOARD_WRAP;
+			tmp++;
+		}
+
+		return tmp;
+	}
+
+    public void moveOrbByIndex(Orb orbToMove)
+    {
+        int x, y;
+
+        x = y = orbToMove.getOrbIndex() + 1;
+        //get x
+        while (x > BOARD_WRAP)
+        {
+            x = x - 6;
+        }
+
+        //get y
+        y = getY(y);
+
+        //Debug.Log (string.Format("displayBoard: Index: {2} X: {0} Y: {1}", x, y, orb.GetComponent<Orb>().index));
+
+        orbToMove.setPosition(new Vector2(x * OFFSET, y * OFFSET));
+    }
+
+	void displayBoard ()
+	{
+		//Debug.Log ("displayBoard: Started.");
+		foreach (GameObject orb in orbs) 
+		{
+			Orb orbToMove = orb.GetComponent<Orb> ();
+			int x, y;
+
+			x = y = orbToMove.getOrbIndex() + 1;
+			//get x
+			while (x > BOARD_WRAP) 
+			{
+				x = x - 6;
+			}
+
+			//get y
+			y = getY(y);
+
+			//Debug.Log (string.Format("displayBoard: Index: {2} X: {0} Y: {1}", x, y, orb.GetComponent<Orb>().index));
+
+			orbToMove.setPosition (new Vector2(x*OFFSET,y*OFFSET));
+		}
+	}
+
+	void switchOrbs (Orb orb1, Orb orb2) //orb1 - moving orb | orb2 - stationary orb
+	{
+        int temp_index = orb2.getOrbIndex();
+    
+		orb2.setOrbIndex (orb1.getOrbIndex ());
+        moveOrbByIndex(orb2);
+
+        orb1.setOrbIndex(temp_index);
+
+        switchOrb = null;
+        switchOrb_S = null;
+	}
+}
---
+++ Assets/Scripts/Orb.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orb : MonoBehaviour
+{
+	string color;
+	public int index;
+	public Vector2 orb_position;
+    bool isMoving = false;
+
+	GameManager gm;
+
+	void Start ()
+	{
+		gm = GameObject.FindGameObjectWithTag ("GameManager").GetComponent<GameManager> ();
+	}
+
+	//void Update (){}
+
+	public void setOrb (int _index, string _color)
+	{
+		index = _index;
+		color = _color;
+	}
+
+	public int getOrbIndex()
+	{
+		return index;
+	}
+
+	public void setOrbIndex(int _index)
+	{
+		index = _index;
+	}
+
+    public void setCatalogedPosition(Vector2 _position)
+    {
+        orb_position = _position;
+    }
+
+	public Vector2 getOrbPosition()
+	{
+		return orb_position;
+	}
+
+	public void setPosition ()
+	{
+		transform.position = orb_position;
+	}
+
+    public void setPosition (Vector2 _position)
+	{
+		transform.position = orb_position = _position;
+		//Debug.Log (string.Format("displayBoard: Index: {2} X: {0} Y: {1}", position.x, position.y, index));
+	}
+
+	void OnMouseDown ()
+	{
+		Debug.Log ("Mouse Down");
+        gameObject.AddComponent<Rigidbody2D>();
+		gameObject.GetComponent<CircleCollider2D> ().radius = 0.01f;
+        isMoving = true;
+		gm.selectedOrb = gameObject;
+		gm.selectedOrb_S = this;
+	}
+
+	void OnMouseUp ()
+	{
+		Debug.Log ("Mouse Up");
+        Destroy(gameObject.GetComponent<Rigidbody2D>());
+		gameObject.GetComponent<CircleCollider2D> ().radius = 0.255f;
+        isMoving = false;
+        gm.selectedOrb = null;
+		gm.selectedOrb_S = null;
+        gm.moveOrbByIndex(this);
+	}
+
+	void OnTriggerEnter2D(Collider2D coll)
+	{
+		Debug.Log ("Collided!");
+		if (coll.gameObject.tag == "Orb" && isMoving) 
+		{
+			gm.switchOrb = coll.gameObject;
+			gm.switchOrb_S = coll.gameObject.GetComponent<Orb> ();
+		}
+	}
+}
